d2065a886feb73c3249c138116ba98d3ba307c46
kathy zhang <kzhang193@gmail.com>
2018-11-14 15:29:39 -0600

    all random vals functioning! yayy!

--- assets/javascript/trial2.js
+++ assets/javascript/trial2.js
@@ -1,6 +1,9 @@
 
 var goalNumber = randomGoal();
-var pickFrom = ["#gem-image1", "#gem-image2", "#gem-image3", "#gem-image4"];
+var gem1 = gemValue();
+var gem2 = gemValue();
+var gem3 = gemValue();
+var gem4 = gemValue();
 var wins = 0;
 var losses = 0; 
 
@@ -12,30 +15,40 @@ function  randomGoal (){
     goalNumber = Math.floor(Math.random()*(102) + 19);
    console.log(goalNumber);
     $("#magic-number").text(goalNumber);
+};
+
+function gemValue() {
+    gem1 = Math.floor(Math.random()*(12) + 1);
+    gem2 = Math.floor(Math.random()*(12) + 1);
+    gem3 = Math.floor(Math.random()*(12) + 1);
+    gem4 = Math.floor(Math.random()*(12) + 1);
+    console.log(gem1);
 }
 
 
-function giveGemValues () {
-
-    for (var i = 0; 1 < pickFrom.length; i++) {
-
-        var gameGem = $("<img>");
-        gameGem.addId(pickFrom[i]);
-        gameGem.attr("data-gemvalue", (Math.floor(Math.random()*(12) + 1))); 
-        gameGem=parseInt(gameGem);
-    }
-
-    $(".gem-image").on("click", function() {
-
-        var gemValue = ($(this).attr("data-gemvalue"));
-        gemValue = parseInt(gemValue);
-        console.log(this);
-
-    })
 
 
+// function giveGemValues () {
 
-} 
+//     for (var i = 0; 1 < pickFrom.length; i++) {
+
+//         var gameGem = $("<img>");
+//         gameGem.addId(pickFrom[i]);
+//         gameGem.attr("data-gemvalue", (Math.floor(Math.random()*(12) + 1))); 
+//         gameGem=parseInt(gameGem);
+//     }
+
+//     $(".gem-image").on("click", function() {
+
+//         var gemValue = ($(this).attr("data-gemvalue"));
+//         gemValue = parseInt(gemValue);
+//         console.log(this);
+
+//     })
+
+
+
+// } 
 
 
 
